d5b183a2e45cbe55fc5a00fb6cc8a9fcc4138eb4
arielzebaze-a11 <ariel.zebaze@facsciences-uy1.cm>
2026-04-16 14:43:10 +0100

    fichier server.js modifier

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const User = sequelize.define('User', {
-    nom: { type: DataTypes.STRING, allowNull: false },
+    name: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, unique: true, allowNull: false },
     solde: { type: DataTypes.FLOAT, defaultValue: 0 }
 });
