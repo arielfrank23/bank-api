a12256c82068b469f62d0da4fcc8b234ab552d3a
arielzebaze-a11 <ariel.zebaze@facsciences-uy1.cm>
2026-04-16 15:05:27 +0100

    Les fichiers modifier et correcte

--- models/User.js
+++ models/User.js
@@ -1,21 +1,12 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
-const User = sequelize.define('User', {
-    // Utilisation de 'name' pour correspondre à tes tests
-    name: { 
-        type: DataTypes.STRING, 
-        allowNull: false 
-    },
-    email: { 
-        type: DataTypes.STRING, 
-        unique: true, 
-        allowNull: false 
-    },
-    solde: { 
-        type: DataTypes.FLOAT, 
-        defaultValue: 0 
-    }
+const User = sequelize.define('User', { // Vérifie si c'est 'User' ou 'Users' selon ta BD
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, unique: true, allowNull: false },
+    solde: { type: DataTypes.FLOAT, defaultValue: 0 }
+}, {
+    tableName: 'Users' // Force Sequelize à regarder la table "Users" que tu as remplie
 });
 
 module.exports = User;
